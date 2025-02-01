bc4dfb4ca491703e6054ff46ec5a46e4e1ddaf8f
ArnoldKebaso <shikukudenno@gmail.com>
2025-02-01 18:54:07 +0300

    Initial commit of TriviaGamesDemo app

---
+++ TriviaGamesDemo/Views/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  TriviaGamesDemo
+//
+//  Created by Arnold Kebaso on 21/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 40){
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .edgesIgnoringSafeArea(.all)
+            .background(Color(red: 0.984313725490196, green: 0.9294117647058824, blue: 0.8470588235294118))
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
